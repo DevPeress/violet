7f5e0fdd1c2de5376d49a943e7d1c862b8fabadd
Peres <fabricioperesdsantos@gmail.com>
2025-07-26 00:16:48 -0300

    Ajuste da APi

--- src/app/api/agricultores/route.ts
+++ src/app/api/agricultores/route.ts
@@ -7,11 +7,12 @@ export async function GET() {
     try {
         const contas = await prisma.user.findMany()
 
-        if (!contas) return NextResponse.json({ status: 200 }, { statusText: "Erro ao pegar os agricultores!" })
+        if (!contas) return NextResponse.json({ message: "Erro ao pegar os agricultores!" }, { status: 200 })
 
         return NextResponse.json(contas)
     } catch(error) {
         console.log("[GET]: ",error)
+        return NextResponse.json({ message: "Erro ao achar agricultores!" }, { status: 200 })
     } finally {
         prisma.$disconnect()
     }
@@ -19,7 +20,34 @@ export async function GET() {
 
 export async function PUT(req: Request) {
     const body = await req.json();
-    console.log(body)
+    const { cpf, tipo, texto } = body as { cpf: string, tipo: string, texto: string }
+
+    try {
+        const conta = await prisma.user.findUnique({
+            where: { cpf: cpf }
+        })
+
+        if (!conta) return NextResponse.json({ message: "Erro ao encontrar dados!" }, { status: 200 })
+
+        const dadosAlterados = { ...conta, [tipo]: texto }
+
+        const update = await prisma.user.update({
+            where: { cpf: cpf },
+            data: { 
+                fullName: dadosAlterados.fullName,
+                cpf: dadosAlterados.cpf,
+                phone: dadosAlterados.phone,
+                birthDate: dadosAlterados.birthDate
+            }
+        })
+
+        return update
+    } catch(error) {
+        console.error("[PUT]: ",error)
+        return NextResponse.json({ message: "Erro ao alterar dados!" }, { status: 200 })
+    } finally {
+        prisma.$disconnect()
+    }
 }
 
 export async function POST(req: Request) {
@@ -42,15 +70,15 @@ export async function POST(req: Request) {
                 }
             })
 
-            if (!criar) return NextResponse.json({ status: 200 }, { statusText: "Erro ao criar agricultor!"  })
+            if (!criar) return NextResponse.json({ message: "Erro ao criar agricultor!"  }, { status: 200 })
 
             return new NextResponse(null, { status: 201 })
         } else {
-            return NextResponse.json({ status: 200 }, { statusText: "CPF ja possui conta!"})
+            return NextResponse.json({ message: "CPF ja possui conta!"}, { status: 200 })
         }
     } catch(error) {
         console.error("[POST]: ", error)
-        return NextResponse.json({ status: 200 }, { statusText: "Erro ao criar!" })
+        return NextResponse.json({ message: "Erro ao criar!" }, { status: 200 })
     } finally {
         prisma.$disconnect()
     }
@@ -65,7 +93,7 @@ export async function DELETE(req: Request) {
             where: { cpf: cpf }
         })
 
-        if (!conta) return NextResponse.json({ status: 200 }, { statusText: "Erro ao encontrar conta para deletar!" })
+        if (!conta) return NextResponse.json({ message: "Erro ao encontrar conta para deletar!" }, { status: 200 })
         
         await prisma.user.delete({
             where: { cpf: cpf }
@@ -74,7 +102,7 @@ export async function DELETE(req: Request) {
         return new NextResponse(null, { status: 204 })
     } catch(error) {
         console.error("[DELETE] :", error)
-        return NextResponse.json({ status: 200 }, { statusText: "Erro ao deletar!" })
+        return NextResponse.json({ message: "Erro ao deletar!" }, { status: 200 })
     } finally {
         prisma.$disconnect()
     }
